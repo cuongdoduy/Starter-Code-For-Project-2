30fa58183137032089eb00bddb90ac5fa1ca7b97
CuongDoDuy <cuongthtb08022004@gmail.com>
2023-06-27 14:59:51 +0700

    First commit

---
+++ application.py
@@ -0,0 +1,23 @@
+from constants import TEAMS,PLAYERS
+import copy
+PlayersData=[]
+TeamsData=[]
+Teams=[]     
+def fetch_data():
+    return
+def clean_data(data):
+    return data
+def add_player():
+    #add Player Here
+    return
+def balance_teams():
+    return 
+def start():
+    return
+if __name__ == "__main__":
+    PlayersData,TeamsData=fetch_data()
+    PlayersData=clean_data(PlayersData)
+    start()
+
+
+
